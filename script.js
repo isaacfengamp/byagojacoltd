c697878ccceda4dae049d1ca32bb100979580d01
Omoz25 <omodingjeff@gmail.com>
2024-04-06 11:25:44 +0300

    sixth

--- script.js
+++ script.js
@@ -20,3 +20,28 @@ navOpenBtn.addEventListener("click", () => {
 navCloseBtn.addEventListener("click", () => {
   nav.classList.remove("openNav");
 });
+(function(){
+	$('.flex-container-mission').waitForImages(function() {
+		$('.spinner-mission').fadeOut();
+	}, $.noop, true);
+	
+	$(".flex-slide-mission").each(function(){
+		$(this).hover(function(){
+			$(this).find('.flex-title-mission').css({
+				transform: 'rotate(0deg)',
+				top: '10%'
+			});
+			$(this).find('.flex-about-mission').css({
+				opacity: '1'
+			});
+		}, function(){
+			$(this).find('.flex-title-mission').css({
+				transform: 'rotate(90deg)',
+				top: '15%'
+			});
+			$(this).find('.flex-about-mission').css({
+				opacity: '0'
+			});
+		})
+	});
+})();
