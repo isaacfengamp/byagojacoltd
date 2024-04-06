473325b115a7c47bd4c6a226564616ad6d434623
Omoz25 <omodingjeff@gmail.com>
2024-04-06 22:00:07 +0300

    next

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+// Wait for the page to load completely
+window.addEventListener('load', function() {
+	// Hide the preloader and display the content after 3 seconds
+	setTimeout(function() {
+	  var preloader = document.getElementById('preloader');
+	  var content = document.getElementById('content');
+	  preloader.style.display = 'none';
+	  content.style.display = 'block';
+	}, 2000); // 3000 milliseconds = 3 seconds
+  });
+  
+
+
 const nav = document.querySelector(".nav"),
   searchIcon = document.querySelector("#searchIcon"),
   navOpenBtn = document.querySelector(".navOpenBtn"),
@@ -45,3 +58,24 @@ navCloseBtn.addEventListener("click", () => {
 		})
 	});
 })();
+
+
+// Function to scroll to the top of the page
+function backToTop() {
+	window.scrollTo({
+	  top: 0,
+	  behavior: 'smooth' // Smooth scrolling
+	});
+  }
+  
+  // Show the "back to top" button when scrolling down
+  window.addEventListener('scroll', function() {
+	var button = document.getElementById('back-to-top');
+	if (window.scrollY > 100) {
+	  button.style.display = 'block';
+	} else {
+	  button.style.display = 'none';
+	}
+  });
+  
+  
